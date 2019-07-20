6cce062407868ce6ad38fc013d3a991b174b6ec2
mediatrice8 <kampiremediatrice@gmail.com>
2019-07-21 01:12:39 +0200

    i create a different variables

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function birth(){
-    
+    var dat = document.getElementById("date").value;
+    var  array = dat.split("-");
+    var arrayNameMan = ['Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame', 'Kwasi'];
+    var arrayNameFemale = ['Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama', 'Akosua'];
+    console.log("month:", dat);
 }
