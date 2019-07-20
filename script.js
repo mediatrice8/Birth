831677ffe91d4731747f384471ac090e66a727ca
mediatrice8 <kampiremediatrice@gmail.com>
2019-07-21 01:13:55 +0200

    i create a different  another variables

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ function birth(){
     var arrayNameMan = ['Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame', 'Kwasi'];
     var arrayNameFemale = ['Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama', 'Akosua'];
     console.log("month:", dat);
+    var dd = parseInt(array[2]);
+    var mm = parseInt(array[1]);
+    var yy = parseInt(array[0]);
 }
