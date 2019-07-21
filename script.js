e09ad770208173a038fdb72b03bd7787aca8d8fa
mediatrice8 <kampiremediatrice@gmail.com>
2019-07-21 11:22:09 +0200

    i display the name

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function birth(){
     var dat = document.getElementById("date").value;
     var  array = dat.split("-");
-    var arrayNameMan = ['Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame', 'Kwasi'];
-    var arrayNameFemale = ['Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama', 'Akosua'];
+    var arrayNameMan = ['Kwasi','Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame',];
+    var arrayNameFemale = ['Akosua','Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama',];
+    // var arrayDay =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
     console.log("month:", dat);
     var dd = parseInt(array[2]);
     var mm = parseInt(array[1]);
@@ -21,12 +22,13 @@ function birth(){
                     result =yy/100 +1;
                     var d =((((result/4)- 2*result -1) +(5*yy/4) +(26*(mm+1)/10) +dd) % 7);
                   var dayNumber = Math.round(d);
-                  console.log(dayNumber);
+                     var Answer = dayNumber;
+                  console.log(Answer);
                   if(document.getElementById("gender").checked){
-                      alert(arrayNameMan[dayNumber - 1]);
+                      alert(arrayNameMan[Answer]);
                   }
                   else if(document.getElementById("gende").checked){
-                    alert(arrayNameFemale[dayNumber - 1]);
+                    alert(arrayNameFemale[Answer]);
                 }else{
                     console.log("none");
                 }
